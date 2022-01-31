9404c70fabb92849a0d9466c057291836c93d6a4
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-31 15:36:24 -0500

    Added a basic signup page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Home from './components/Home';
+import Signup from './components/Signup';
 import Login from './components/Login';
+import Logout from './components/Logout';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import './App.css';
 
 function App() {
   
-  useEffect(() => {
-    localStorage.setItem('token', 'something');
-  }, [])
   return (
     <Router>
+      <header>
+        <Link to='/' className='header-title'>AFRICAN MARKETPLACE</Link>
+        <nav className='links'>
+          <Link to='/login'>LOGIN</Link>
+          <Link to='/signup'>SIGNUP</Link>
+        </nav>
+      </header>
       <Switch>
-        <ProtectedRoute path='/protected' component={Login}/>
+        <ProtectedRoute path='/logout' component={Logout}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/signup' component={Signup}/>
         <Route path='/' component={Home}/>
       </Switch>
     </Router>
