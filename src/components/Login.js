08ccf2646fac2597260fd37d776b9801e525cecb
Uros Ivanovic <urosivanovic@Uross-MBP-116.lan>
2022-01-31 15:46:49 -0500

    login form done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = (props) => {
+const Login = () => {
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    })
+
+    const handleChange = e => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(user);
+    }
+
     return (
         <>
             <h1>Login</h1>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label> Username:
+                    <input
+                        type='text'
+                        name='username'
+                        value={user.username}
+                        onChange={handleChange} 
+                    />
+                    </label>
+
+                    <label>Password:
+                    <input
+                        type='password'
+                        name='password'
+                        value={user.password}
+                        onChange={handleChange}    
+                    />
+                    </label>
+                    <button>Log In</button>
+                </form>
+            </div>
         </>
     )
 }
