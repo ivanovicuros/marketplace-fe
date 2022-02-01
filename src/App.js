eb5e49c3d42a7f230171d1a62061212b42f570ad
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-31 23:14:41 -0500

    Made the footer with some dummy links

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ import Styled from 'styled-components';
 
 import './App.css';
 
+const StyledApp = Styled.div`
+  a{
+    color: white;
+    font-size: 1.4rem;
+    font-weight: bold;
+    text-decoration: none;
+  }
+`
+
 const Header = Styled.header`
   position: absolute;
   background-color: #2E3842;
@@ -24,13 +33,6 @@ const Header = Styled.header`
     width: 50%;
     padding: 0 2%;
   }
-
-  a{
-    color: white;
-    font-size: 1.4rem;
-    font-weight: bold;
-    text-decoration: none;
-  }
 `
 
 const Nav = Styled.nav`
@@ -42,38 +44,44 @@ const Nav = Styled.nav`
 const Footer = Styled.footer`
   height: 10vh;
   background-color: #1D242AE6;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
 `
 
 function App() {
   
   return (
     <Router>
+      <StyledApp>
+        <Header>
+          <Link to='/' className='header-title'>AFRICAN MARKETPLACE</Link>
 
-      <Header>
-        <Link to='/' className='header-title'>AFRICAN MARKETPLACE</Link>
+          <Nav className='links'>
+            <Link to='/login'>LOGIN</Link>
+            <Link to='/signup'>SIGNUP</Link>
+            <Link to='/logout'>LOGOUT</Link>
+            <Link to='/item-form'>ADD ITEM</Link>
+            <Link to='/profile'>PROFILE</Link>
+          </Nav>
+        </Header>
 
-        <Nav className='links'>
-          <Link to='/login'>LOGIN</Link>
-          <Link to='/signup'>SIGNUP</Link>
-          <Link to='/logout'>LOGOUT</Link>
-          <Link to='/item-form'>ADD ITEM</Link>
-          <Link to='/profile'>PROFILE</Link>
-        </Nav>
+        <Switch>
+          <ProtectedRoute path='/logout' component={Logout}/>
+          <ProtectedRoute path='/item-form' component={ItemForm}/>
+          <Route path='/profile' component={Profile} />
+          <Route path='/login' component={Login}/>
+          <Route path='/signup' component={Signup}/>
+          <Route path='/' component={Home}/>
+        </Switch>
 
-      </Header>
-
-      <Switch>
-        <ProtectedRoute path='/logout' component={Logout}/>
-        <ProtectedRoute path='/item-form' component={ItemForm}/>
-        <Route path='/profile' component={Profile} />
-        <Route path='/login' component={Login}/>
-        <Route path='/signup' component={Signup}/>
-        <Route path='/' component={Home}/>
-      </Switch>
-
-      <Footer>
-        <i className="fab fa-twitter"></i>
-      </Footer>
+        <Footer>
+          <Link to='/about'>ABOUT US</Link>
+          <Link to='/team'>MEET THE TEAM</Link>
+          <Link to='/faq'>FAQ</Link>
+        </Footer>
+      </StyledApp>
     </Router>
   );
 }
