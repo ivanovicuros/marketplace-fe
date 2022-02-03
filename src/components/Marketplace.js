4aa1569a81142ebb395593c4b987cc5db1c5db5f
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2022-02-02 19:06:58 -0500

    styling marketplace in progress

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,5 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "./utils/axiosWithAuth";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+margin-top: 0;
+`
+
+const StyledWrapper = styled.div`
+    padding: 5%;
+    margin: 3%;
+    margin-top: 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+
+
+`
+const StyledCard = styled.div`
+    
+    cursor: pointer;
+     
+`
+const StyledImg = styled.img`
+    width: 85%;
+    height: 85%;
+    margin: 15px;
+    border: 1px solid #555555;
+    border-radius: 5px;
+    box-sizing: border-box;
+`
 
 const Marketplace = () => {
     const [items, setItems] = useState([]);
@@ -7,23 +37,30 @@ const Marketplace = () => {
     useEffect(() => {
         axiosWithAuth().get('/items')
         .then(resp => {
-            console.log(resp)
             setItems([
                 ...items,
-                resp.data
+                ...resp.data
             ])
         })
         .catch(err => {
             console.log({err})
         })
     }, [])
-
     console.log(items)
 
     return(
-        <>
-
-        </>
+        <StyledContainer>
+            <StyledWrapper>
+                {items.map(item => (
+                    <StyledCard className="item-card" key={item.item_id}>
+                        <StyledImg src={item.image} />
+                        <p>{item.name}</p>
+                        <p>$: {item.price}</p>
+                        <p>{item.description}</p>
+                    </StyledCard>
+                ))}
+            </StyledWrapper>
+        </StyledContainer>
     )
 }
 
