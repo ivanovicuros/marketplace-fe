d8d1013f36e0bc5c4211df08e03eec62e3abd7f5
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-02-04 16:17:39 -0500

    Made the update item button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import axiosWithAuth from './utils/axiosWithAuth';
+import { StyledButton } from './styles/Form';
+import { useHistory } from 'react-router-dom';
 
+const StyledButtons = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    button{
+        width: 40%;
+    }
+
+    #delete{
+        background-color: #bb2222;
+    }
+`
 
 const StyledCard = styled.div`
     text-transform: capitalize;
@@ -17,6 +31,7 @@ const StyledImg = styled.img`
 
 const Item = (props) => {
     const { item, marketplace, setItems } = props;
+    const { push } = useHistory();
 
    const handleDelete = () => {
        console.log('click')
@@ -27,13 +42,21 @@ const Item = (props) => {
        .catch(err => console.log(err))
    }
 
+   const handleUpdate = () => {
+       push(`/item-form/${item.item_id}`);
+   }
+
     return(
         <StyledCard className="item-card">
             <StyledImg src={item.image} alt={item.name} />
             <p>{item.name}</p>
             <p>${item.price}</p>
             <p>{item.description}</p>
-            {!marketplace && <button onClick={handleDelete}>Delete</button>}
+            {!marketplace && 
+            <StyledButtons>
+                <StyledButton onClick={handleUpdate}>Update</StyledButton>
+                <StyledButton onClick={handleDelete} id='delete'>Delete</StyledButton>
+            </StyledButtons>}
 
         </StyledCard>
     )
